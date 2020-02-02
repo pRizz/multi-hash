f2b52af0b1dcbb52633a59625a08ed12dff06038
Peter Ryszkiewicz <pRizz@users.noreply.github.com>
2020-02-01 23:10:45 -0600

    Make build

---
+++ docs/precache-manifest.2543fa82dc6d90b5d4a273ebc09f0055.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2d57adb1352505ddd650682141c4ead",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59f21e1ce6f55ba3902d",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "14f3f31711eb62f6df14",
+    "url": "/static/js/2.0b8b2bf0.chunk.js"
+  },
+  {
+    "revision": "f304bc7ec7dc324c88d8c079f551429e",
+    "url": "/static/js/2.0b8b2bf0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59f21e1ce6f55ba3902d",
+    "url": "/static/js/main.b4d2df87.chunk.js"
+  },
+  {
+    "revision": "186cdffa46bab390c760",
+    "url": "/static/js/runtime-main.11b2d2b1.js"
+  }
+]);
